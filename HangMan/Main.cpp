999f44f2bb0d685468a0a857bf6eb9356f2490ad
Luc Thomas <lucsprojectsNL@gmail.com>
2016-04-16 09:58:02 +0200

    More comment for logic added

--- HangMan/Main.cpp
+++ HangMan/Main.cpp
@@ -10,13 +10,26 @@ int main() {
 	std::string Guess;
 
 	//While you are NOT dead or the word is NOT guessed
-	std::cout << "Please type in a letter!" << std::endl;
-	std::getline(std::cin, Guess);
+	while (true) {
+		std::cout << "Please type in a letter!" << std::endl;
+		std::getline(std::cin, Guess);
 
-	//Check guess validity (is it a single letter?
-	//If the guess is valid
-		//Submit Guess for checking against the word
-	//If not
-		//Return appropreate error
+		//Check guess validity (is it a single letter?
+		//If the guess is valid
+			//Submit Guess for checking against the word
+		//If not
+			//Return appropreate error
 
+		//Check if you are dead
+		//If you are dead
+			//Tell the player they lost and this round
+		//If not
+			//Do Nothing
+		
+		//Check if the word is guessed
+		//If yes
+			//Congradulate the player and end round
+		//If no
+			//Do Nothing
+	}
 }
